9762c11e2be58423567005fe1a5dce23854efc70
Ayaal Kaplin <uraankhayayaal@gmail.com>
2022-04-21 23:15:27 +0900

    add owl asset class

--- src/widgets/partner/assets/PartnerAsset.php
+++ src/widgets/partner/assets/PartnerAsset.php
@@ -9,15 +9,13 @@ class PartnerAsset extends AssetBundle
     public $sourcePath = '@ityakutia/partner/widgets/partner/assets/src/';
     
     public $css = [
-        'css/owl.carousel.min.css',
-        'css/owl.theme.default.min.css',
         'css/widget.css',
     ];
     public $js = [
-        'js/owl.carousel.min.js',
         'js/widget.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
+        'ityakutia\owl\OwlAsset',
     ];
 }
